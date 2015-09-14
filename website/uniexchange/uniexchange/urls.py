df264a84a8d8e99ffe05c46c2c777abbb5c07caf
Henry Chen <henryc345@gmail.com>
2015-09-14 11:09:54 +0800

    More work on django tutorial

--- website/uniexchange/uniexchange/urls.py
+++ website/uniexchange/uniexchange/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from uniexchange.views import hello
+from uniexchange.views import hello, current_datetime, hours_ahead
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/$', hello),
+    url(r'^hello$', hello),
     # url(r'^$', index),
-    url(r'^time/$', current_datetime),
+    url(r'^time$', current_datetime),
+    url(r'^time/plus/(\d{1,2})$', hours_ahead),
 ]
